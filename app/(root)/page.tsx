e67881520062f2dff9a164107ade1a34ce18aee3
NANA KOJO <nanakojo@MacBook-Pro.local>
2025-11-11 21:33:22 -0800

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,58 @@
+import StartupCard from "@/components/ui/StartupCard";
+import SearchForm from "../../components/ui/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Adrian" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://media.kasperskydaily.com/wp-content/uploads/sites/92/2022/11/28103136/robots-and-cybersecurity-Featured.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          PITCH YOUR STARTUP, <br />
+          CONNECT WITH ENTERPRENEURS
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
+  );
+}
